54cf75d91078e4289a5b3fd52073ab44558e7a1f
nitaj96a <nitaj1996@gmail.com>
2018-11-04 16:36:06 +0100

    init project

---
+++ src/main/java/com/n96a/ebooks/EBookRepoApplication.java
@@ -0,0 +1,12 @@
+package com.n96a.ebooks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EBookRepoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EBookRepoApplication.class, args);
+	}
+}
