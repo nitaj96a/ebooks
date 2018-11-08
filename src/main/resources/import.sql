bcc78a59a10eed1dbe11b8b8d0a1754c75ff2175
nitaj96a <nitaj1996@gmail.com>
2018-11-08 01:44:38 +0100

    added a bunch of books in the init SQL script
    in english, french, german

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -2,18 +2,45 @@ INSERT INTO users(firstname,lastname,username,password, usertype) VALUES ('Satos
 INSERT INTO users(firstname,lastname,username,password, usertype) VALUES ('Kaz','Hirai','kaz','sifra1','regular');
 INSERT INTO users(firstname,lastname,username,password, usertype) VALUES ('Gavin', 'Andresen','gav','sifra1', 'regular');
 
+INSERT INTO users(firstname,lastname,username,password, usertype) VALUES ('Pierre', 'Dubois','pier1','sifra1', 'regular');
+
+INSERT INTO users(firstname,lastname,username,password, usertype) VALUES ('Klaus', 'Wagner','kwag','sifra1', 'regular');
+
 INSERT INTO languages(name) VALUES ('English');
 INSERT INTO languages(name) VALUES ('French');
-INSERT INTO languages(name) VALUES ('Serbian');
-INSERT INTO languages(name) VALUES ('Japanese');
-INSERT INTO languages(name) VALUES ('Russian');
+INSERT INTO languages(name) VALUES ('German');
 
 INSERT INTO categories(name) VALUES('Engineering');
-INSERT INTO categories(name) VALUES('Fiction');
-INSERT INTO categories(name) VALUES('Non-fiction');
+INSERT INTO categories(name) VALUES('Scince-Fiction');
 INSERT INTO categories(name) VALUES('Programming');
+INSERT INTO categories(name) VALUES('Fiction');
+INSERT INTO categories(name) VALUES('History');
+INSERT INTO categories(name) VALUES('For Kids');
+INSERT INTO categories(name) VALUES('Philosophy');
 
-INSERT INTO ebooks(title, author, publicationyear, keywords, filename, thumbnailpath, user_id, category_id, language_id) VALUES('Pro Git', 'Scott Chacon, Ben Straub', 2018,'Git, Version control, Workflow' ,'progit_v2.1.3.pdf','progit2.png',1, 4, 1);
-INSERT INTO ebooks(title, author, publicationyear, keywords,filename, thumbnailpath, user_id, category_id, language_id) VALUES('A First Course in Electrical and Computer Engineering', 'Louis Scharf', 2009, 'Complex numbers, Phasors, Linear Algebra, Vector graphics, Filtering','a-first-course-in-electrical-and-computer-engineering-2.3.pdf', 'a-first-course-in-electrical-and-computer-engineering.jpg',3, 1, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords, filename, mime, thumbnailpath, user_id, category_id, language_id) VALUES('Pro Git', 'Scott Chacon, Ben Straub', 2018,'Git, Version control, Workflow' ,'progit_v2.1.3.pdf','application/pdf','progit2.png',1, 3, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('A First Course in Electrical and Computer Engineering', 'Louis Scharf', 2009, 'Complex numbers, Phasors, Linear Algebra, Vector graphics, Filtering','a-first-course-in-electrical-and-computer-engineering-2.3.pdf', 'application/pdf','a-first-course-in-electrical-and-computer-engineering.jpg',3, 1, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Erewhon', 'Samuel Butler', 1872, 'Machines, Evolution, AI, Utopia','erewhon.txt', 'text/plain','Erewhon_Cover.jpg',2, 2, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('The Island of Doctor Moreau', 'H. G. Wells', 1896, 'Mad Scientist, Shipwrecked, Genetic Engineering','islandofdoctormoreau.txt', 'text/plain','IslandOfDrMoreau.jpg',3, 2, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('An Introduction to Information Retrieval', 'Christopher D. Manning, Prabhakar Raghavan, Hinrich Schutze', 2009, 'IR, Index, Relevance Feedback, Text Classification, Clustering','irbook.pdf', 'application/pdf','iir.jpg',3, 3, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('On the Origin of Clockwork and the Compass', 'Derek J. de Solla Price', 2009, 'IR, Index, Relevance Feedback, Text Classification, Clustering','originofcpmdc.txt', 'text/plain','originofcpmdc.jpg',1, 1, 1);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Dracula', 'Bram Stoker', 1897, 'Vampire, Van Helsing','dracula.txt', 'text/plain','dracula.jpg',3, 4, 1);
+
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('La comédie humaine volume I', 'Honoré de Balzac', 1830, '','la comedie humane.txt', 'text/plain','la comedie humaine.jfif',4, 4, 2);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Les quatre livres de philosophie morale et politique', 'Confucius et Mencius', -553, 'to be indexed','Les quatre livres de philosophie morale et politique.txt', 'text/plain','Les quatre livres de philosophie morale et politique.jpg',4, 5, 2);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Le Cardinal de Richelieu', 'Hyacinthe Corne', 1856, 'to be indexed','cardinal.txt', 'text/plain','cardinal.jpg',4, 5, 2);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Jacques le fataliste et son maître', 'Denis Diderot', 1796, 'to be indexed','jacques.txt', 'text/plain','jacques.jpeg',4, 4, 2);
+
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Märchen für Kinder', 'Hans Christian Andersen', 2006, 'to be indexed','kinder.txt', 'text/plain','kinder.jpg',5, 6, 3);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Kritik der reinen Vernunft', 'Immanuel Kant', 1781, 'to be indexed','kritik der reinen Vernunft.txt', 'text/plain','kritik der reinen Vernunft.jpg',5, 7, 3);
+INSERT INTO ebooks(title, author, publicationyear, keywords,filename, mime,thumbnailpath, user_id, category_id, language_id) VALUES('Zwischen Himmel und Erde', 'Otto Ludwig', 1855, 'to be indexed','himmelunderde.txt', 'text/plain','himmelunderde.jpg',5, 4, 3);
+
+
+
+
+
+
+
+
 
 
